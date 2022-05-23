c0efbb411bc6c133ea4fbbccd1beb4aba0a5bf67
anantapk03 <anantapk03@gmail.com>
2022-05-23 23:30:32 +0700

    ananta version, hitung bangun datar

---
+++ latihan/persegi.py
@@ -0,0 +1,17 @@
+from penghitung_bangundatar import HitungBangunDatar
+
+class Persegi (HitungBangunDatar) :
+    
+    def __init__(self,sisi:float) :
+        super().__init__(sisi) 
+    
+    def hitung_luas (self) :
+        print (self.get_sisi()*self.get_sisi())
+    
+    def hitung_keliling(self) :
+        print (self.get_sisi()*4)  
+    
+
+
+persegi = Persegi (4)
+persegi.hitung_luas()
