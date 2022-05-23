dc0d87fcdcf24cf358ff79eba350097936e298cf
anantapk03 <anantapk03@gmail.com>
2022-05-23 22:58:44 +0700

    Burung berenang, bersama ananta sambil main

---
+++ contoh/main.py
@@ -0,0 +1,8 @@
+from penguin import Penguin 
+from eagle import EaglesBird  
+
+eagle = EaglesBird()
+penguin = Penguin ()
+
+eagle.fly()
+penguin.swim()
---
+++ contoh/penguin.py
@@ -0,0 +1,8 @@
+from swimming_bird import SwimmingBird 
+
+class Penguin (SwimmingBird) :
+    def swim (self):
+        print ("Penguin swim to the ocean") 
+
+    
+    
